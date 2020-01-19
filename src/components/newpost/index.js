37e937f75f5cf4c0fc4b4942075f1a03f9c0d437
Vinicius CFGroup <vz@cfgroup.com.br>
2020-01-19 18:53:29 -0300

    upload images newpost

--- src/components/newpost/index.js
+++ src/components/newpost/index.js
@@ -11,10 +11,14 @@ class NewPost extends Component {
       subtitle: '',
       description: '',
       image: '',
-      alert: ''
+      file: null,
+      alert: '',
+      progress: 0
     };
-  this.addPost    = this.addPost.bind(this);
-  this.onAddPost  = this.onAddPost.bind(this);
+  this.addPost      = this.addPost.bind(this);
+  this.onAddPost    = this.onAddPost.bind(this);
+  this.handleFile   = this.handleFile.bind(this);
+  this.onHandleFile = this.onHandleFile.bind(this);
   }
 
   async  componentDidMount(){
@@ -28,13 +32,55 @@ class NewPost extends Component {
     })
   }
 
+  handleFile = async (e) => {
+    if (e.target.files[0]) {
+
+      const selectedFile = e.target.files[0];
+
+      if( selectedFile.type === 'image/png' || selectedFile.type === 'image/jpeg'  ){
+         await this.setState({file: selectedFile});
+         this.onHandleFile();
+      }else {
+        this.setState({alert: 'Envie uma imagem  tipo JPG ou PNG'});
+        this.setState({file: null});
+        return null;
+      }
+
+    }
+  }
+
+  onHandleFile = async () => {
+    const { file  }   = this.state;
+    const currentUid  = firebase.getCurrentUid();
+    const uploadTaks = firebase.storage
+    .ref(`images/${currentUid}/${file.name}`)
+    .put(file);
+
+    await uploadTaks.on('state_changed',
+    (snapshot)=>{
+      const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+      this.setState({progress});
+    },(err)=>{
+      console.log(err);
+    },
+    ()=>{
+      firebase.storage.ref(`images/${currentUid}`)
+      .child(file.name).getDownloadURL()
+      .then(url => {
+        this.setState({image: url});
+      })
+
+    })
+
+  }
+
   addPost(e){
     e.preventDefault();
     this.onAddPost();
   }
 
   onAddPost = async () =>{
-    if(this.state.titulo !== '' && this.state.image !== '' && this.state.description !== ''){
+    if(this.state.titulo !== '' && this.state.image !== '' && this.state.description !== '' && this.state.file !== null ){
       let posts = firebase.app.ref('posts');
       let key = posts.push().key;
       await posts.child(key).set({
@@ -64,9 +110,14 @@ class NewPost extends Component {
           <label>Sub-título:</label>
           <input type="text" placeholder="Se houver digite o sub-titulo"
           value={this.state.subtitle} onChange={(e)=> this.setState({subtitle: e.target.value})} />
-          <label>URL da imagem:</label>
-          <input type="text" placeholder="Cole aqui a URL"
-          value={this.state.image} onChange={(e)=> this.setState({image: e.target.value})} />
+        <label>Imagem:</label>
+          <input type="file"
+          onChange={this.handleFile} />
+          { this.state.image !== '' ?
+            <img src={this.state.image} width="250" height="150" alt="Capa do Post" />
+            :
+            <progress value={this.state.progress} max="100" />
+          }
           <label>Descrição:</label>
           <textarea type="text" placeholder="Digite a descrição..."
           value={this.state.description} onChange={(e)=> this.setState({description: e.target.value})} />
