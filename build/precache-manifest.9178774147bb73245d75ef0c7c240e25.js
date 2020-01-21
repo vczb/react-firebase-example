758caac7e7707d6dcd495403e8ffe098b7d472aa
Vinicius CFGroup <vz@cfgroup.com.br>
2020-01-21 00:18:16 -0300

    deploy heroku

---
+++ build/precache-manifest.9178774147bb73245d75ef0c7c240e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab261d4580813b38324cb8a0f2f2763",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb7533405e3ac4fcf6bc",
+    "url": "/static/css/main.7deec768.chunk.css"
+  },
+  {
+    "revision": "8b4f9e5912d10518522e",
+    "url": "/static/js/2.25b74c33.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/static/js/2.25b74c33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb7533405e3ac4fcf6bc",
+    "url": "/static/js/main.7a36e3f8.chunk.js"
+  },
+  {
+    "revision": "e6e78466b638df2e3d68",
+    "url": "/static/js/runtime-main.ef29f233.js"
+  }
+]);
